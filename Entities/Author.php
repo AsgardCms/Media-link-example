0ec3173150b79db20f6f85cf16ac132758214347
Nicolas Widart <n.widart@gmail.com>
2017-07-03 11:05:14 +0200

    Adding the media relation trait on the model

--- Entities/Author.php
+++ Entities/Author.php
@@ -3,9 +3,11 @@
 namespace Modules\Medialinkexample\Entities;
 
 use Illuminate\Database\Eloquent\Model;
+use Modules\Media\Support\Traits\MediaRelation;
 
 class Author extends Model
 {
+    use MediaRelation;
     protected $table = 'medialinkexample__authors';
     protected $fillable = ['name'];
 }
