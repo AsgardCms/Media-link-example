ebf776d9b2eb9558efdb25193e4611d245abb282
Nicolas Widart <n.widart@gmail.com>
2017-07-03 11:17:41 +0200

    Overwriting create/update/delete methods on repository to trigger our events

--- Repositories/Eloquent/EloquentAuthorRepository.php
+++ Repositories/Eloquent/EloquentAuthorRepository.php
@@ -2,9 +2,37 @@
 
 namespace Modules\Medialinkexample\Repositories\Eloquent;
 
+use Modules\Medialinkexample\Events\AuthorWasCreated;
+use Modules\Medialinkexample\Events\AuthorWasDeleted;
+use Modules\Medialinkexample\Events\AuthorWasUpdated;
 use Modules\Medialinkexample\Repositories\AuthorRepository;
 use Modules\Core\Repositories\Eloquent\EloquentBaseRepository;
 
 class EloquentAuthorRepository extends EloquentBaseRepository implements AuthorRepository
 {
+    public function create($data)
+    {
+        $author = $this->model->create($data);
+
+        event(new AuthorWasCreated($author, $data));
+
+        return $author;
+    }
+
+    public function update($author, $data)
+    {
+        $author->update($data);
+
+        event(new AuthorWasUpdated($author, $data));
+
+        return $author;
+    }
+
+    public function destroy($author)
+    {
+        event(new AuthorWasDeleted($author));
+
+        return $author->delete();
+    }
+
 }
